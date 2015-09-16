6e4bfca8ffaef847519d484b0f86b1fc02ae474a
ucrpractica <angealvarez77@gmail.com>
2015-09-16 09:36:24 -0600

    capa

---
+++ Capa.Models/Pacientes/Alergias.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa.Models.Pacientes
+{
+    public enum Alergias
+    {
+        Aspirina = 1,
+        Penicilina = 2,
+        Sulfas = 3,
+        OtrosMedicamentos = 4
+    }
+}
