bfaa23fe505f08e51ebe54d6434091812a74b7cc
ErikaAGarcia <eri.89.91@gmail.com>
2015-09-18 17:46:20 -0500

    mi cambio

--- Capa.Formularios/Usuarios/NuevoUsuario.cs
+++ Capa.Formularios/Usuarios/NuevoUsuario.cs
@@ -23,48 +23,17 @@ namespace Capa.Formularios
 
         }
 
-        private void Leer(string dato)
-        {
-            try
-            {
-                dgv.DataSource = Busines.UsuariosMetodos.Listar(dato);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-        }
+       
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            dgv.AutoGenerateColumns = false;
-            Leer("");
+          
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var oEntidad = new Capa.Models.NuevoUsuario();
-            if (usuario != null)
-                
-            oEntidad.Nombre_Completo = textNombreCompleto.Text.Trim();
-            oEntidad.Nombre_Usuario = textUsuario.Text.Trim();
-            oEntidad.Tipo_Usuario= Models.TipoUsuario.AdministradorGeneral;
-            oEntidad.Fecha_Creacion = DateTime.Now;
-            oEntidad.Correo = textCorreo.Text.Trim();
-            oEntidad.Contraseña = textContraseña.Text.Trim();
-          
 
-            try
-            {
-                Capa.Busines.UsuariosMetodos.Grabar(oEntidad);
-              
-                Leer("");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            finally { oEntidad = null; }
+
         }
     }
 }
