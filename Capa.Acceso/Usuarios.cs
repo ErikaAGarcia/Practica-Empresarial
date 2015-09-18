588ea18873bbbbfb81766a0145822f31ebee0681
ErikaAGarcia <eri.89.91@gmail.com>
2015-09-18 17:44:05 -0500

    mio

--- Capa.Acceso/Usuarios.cs
+++ Capa.Acceso/Usuarios.cs
@@ -12,97 +12,93 @@ namespace Capa.Acceso
     public class Usuarios
     {
 
-        public static bool Grabar(Capa.Models.NuevoUsuario pEntidad)
+
+        public static Capa.Models.NuevoUsuario NuevoUsuario(MySqlDataReader entidad)
         {
-            using (var cn = new MySqlConnection(Conexion.LeerCC))
+            var e = new Capa.Models.NuevoUsuario();
 
+            e.Id_Usuario = Convert.ToInt32(entidad["Id_Usuario"]);
+            e.Nombre_Completo = Convert.ToString(entidad["Nombre_Completo"]);
+            e.Nombre_Usuario = Convert.ToString(entidad["Nombre_Usuario"]);
+            e.Fecha_Creacion = Convert.ToDateTime(entidad["Fecha_Creacion"]);
+            e.Correo = Convert.ToString(entidad["Correo"]);
+            e.Contraseña = Convert.ToString(entidad["Contraseña"]);
+            e.Tipo_Usuario = Models.TipoUsuario.AdministradorGeneral;
+            return e;
+        }
+        //listar
+        public static List<Models.NuevoUsuario> ListaUsuarios()
+        {
+            var lista = new List<Models.NuevoUsuario>();
+            using (MySqlConnection cn = new MySqlConnection(Conexion.Cadena))
             {
-                // Contamos cuantos usuarios existen
-                using (var cmd = new MySqlCommand(@"select ifnull(count(Nombre_Completo),0) from Clinica where Nombre_Completo=@Nombre ", cn))
+                var consulta = "select * from Usuarios";
+                var cmd = new MySqlCommand(consulta, cn);
+                cn.Open();
+                var dr = cmd.ExecuteReader();
+                while (dr.Read())
                 {
-                    //Asignar valores a los parametros
-                    cmd.Parameters.AddWithValue("Nombre", pEntidad.Nombre_Completo);
-                    cmd.Parameters.AddWithValue("Usuario", pEntidad.Nombre_Usuario);
-                    cmd.Parameters.AddWithValue("TipoUsuario", pEntidad.Tipo_Usuario);
-                    cmd.Parameters.AddWithValue("email", pEntidad.Correo);
-                    cmd.Parameters.AddWithValue("Pass", pEntidad.Contraseña);
-                    cmd.Parameters.AddWithValue("Fecha", pEntidad.Fecha_Creacion);
-
-
-                    cn.Open();
-                    // Ejecutamos el comando y verificamos si el resultado es mayor a cero actualizar, caso contrario insertar
-                    if (Convert.ToInt32(cmd.ExecuteScalar()) > 0)
-                    {
-                        //// Si es mayor a cero, quiere decir que existe al menos un registro con los datos ingresados
-                        //// Entonces antes de actualizar, hacer las siguientes comprobaciones
-                        //if (pEntidad.Nombre_Completo == "")
-                        //    throw new Exception("El amigo(a) ya esta registrado en el sistema, verifique los datos por favor!...");
-
-                        // Verifica si ya existe un registro con el mismo nombre del distrito
-                        cmd.CommandText = @"select ifnull(count(Nombre_Completo),0) from Usuarios where Nombre_Completo<>@nombre ";
-                        if (Convert.ToInt32(cmd.ExecuteScalar()) > 0)
-                            throw new Exception("No se puede grabar un valor duplicado, verifique los datos por favor!...");
-
-                        // Si las comprobaciones anteriores resultaron ser falsa, entonces actualizar
-                        cmd.CommandText = @"insert into Usuarios (Nombre_Completo,Nombre_Usuario,Tipo_Usuario,Correo,Contraseña,Fecha_Creacion) values (@Nombre, @Usuario, @TipoUsuario, @email, @Pass, @Fecha);";
-                    }
-
-                    // Ejecutamos el comando que puede ser para update o insert
-                    return Convert.ToBoolean(cmd.ExecuteNonQuery());
+                    lista.Add(NuevoUsuario(dr));
                 }
+                return lista;
+            }
+        }
+        //agregar
+        public static bool Agrega(Models.NuevoUsuario User)
+        {
+
+            using (MySqlConnection cn = new MySqlConnection(Conexion.Cadena))
+            {
+                var consulta = "insert into usuario values (@Nombre_Completo,@Nombre_Usuario,@Fecha_Creacion,@Correo,@Contraseña)";
+                var cmd = new MySqlCommand(consulta, cn);
+                cmd.Parameters.AddWithValue("@Nombre_Completo", User.Nombre_Completo);////falta algo creo
+                cmd.Parameters.AddWithValue("@Nombre_Usuario", User.Nombre_Usuario);////falta algo creo
+                cmd.Parameters.AddWithValue("@Tipo_Usuario", User.Tipo_Usuario);
+                cmd.Parameters.AddWithValue("@Fecha_Creacion", User.Fecha_Creacion);////falta algo creo
+                cmd.Parameters.AddWithValue("@Correo", User.Correo);////falta algo creo
+                cmd.Parameters.AddWithValue("@Contraseña", User.Contraseña);////falta algo creo
+                cn.Open();
+                return Convert.ToBoolean(cmd.ExecuteNonQuery());
             }
         }
 
-        //public static bool Eliminar(Entidades.Amigo pEntidad)
-        //{
-        //    using (var cn = new MySqlConnection(conexion.LeerCC))
-        //    {
-        //        // Como nadie depende de esta entidad (amigos) no habra comprobaciones de dependencia
-        //        using (var cmd = new MySqlCommand(@"delete from amigos where codamigo=@cod;", cn))
-        //        {
-        //            cmd.Parameters.AddWithValue("cod", pEntidad.codamigo);
-
-        //            cn.Open();
-        //            // Ejecuta el comando
-        //            return Convert.ToBoolean(cmd.ExecuteNonQuery());
-        //        }
-        //    }
-        //}
-
-        public static List<Capa.Models.NuevoUsuario> Leer(string dato)
+        //actualizar
+        public static bool actualizar(Models.NuevoUsuario User)
         {
-            // Crea un obj. lista de tipo Distrito
-            var lista = new List<Capa.Models.NuevoUsuario>();
-            // Crear el objeto de conexion
-            using (var cn = new MySqlConnection(Conexion.LeerCC))
+            using (MySqlConnection cn = new MySqlConnection(Conexion.Cadena))
             {
-                // crear el comando
-                using (var cmd = new MySqlCommand("select Nombre_Completo,  from Usuarios where Nombre_Completo like Concat(@Nombre, '%');", cn))
-                {
-                    //Asignar valores a los parametros
-                    cmd.Parameters.AddWithValue("Nombre", dato);
+                var consulta = "update usuario set Nombre_Completo=@Nombre_Completo,Nombre_Usuario=@Nombre_Usuario,@Tipo_Usuario=Tipo_Usuario,Fecha_Creacion=@Fecha_Creacion,Correo=@Correo,Contraseña=@Contraseña";
+                var cmd = new MySqlCommand(consulta, cn);
+                cmd.Parameters.AddWithValue("@Nombre_Completo", User.Nombre_Completo);////falta algo creo
+                cmd.Parameters.AddWithValue("@Nombre_Usuario", User.Nombre_Usuario);////falta algo creo
+                cmd.Parameters.AddWithValue("@Tipo_Usuario", User.Tipo_Usuario);
+                cmd.Parameters.AddWithValue("@Fecha_Creacion", User.Fecha_Creacion);////falta algo creo
+                cmd.Parameters.AddWithValue("@Correo", User.Correo);////falta algo creo
+                cmd.Parameters.AddWithValue("@Contraseña", User.Contraseña);////falta algo creo
+                cmd.Parameters.AddWithValue("@Id_Usuario", User.Id_Usuario);////falta algo creo
 
-                    // Abrir el objeto de conexion
-                    cn.Open();
-                    using (var dr = cmd.ExecuteReader())
-                    {
-                        while (dr.Read())
-                        {
-                            // Crea un objeto del Usuario
-                            var usuarios = new Capa.Models.NuevoUsuario();
-                            usuarios.Nombre_Completo= "Nombre";
-                            
-                            // El objeto Usuario es agregado a la lista
-                            lista.Add(usuarios);
-                            usuarios = null;
-                        }
-                    }
+                cn.Open();
+                return Convert.ToBoolean(cmd.ExecuteNonQuery());
 
-    
-            // Retorna una lista de datos
-            return lista;
-                }
+
             }
-        
-    
-}
+        }
+
+        //Eliminar
+        public static bool Eliminar(Models.NuevoUsuario User)
+        {
+            using (MySqlConnection cn = new MySqlConnection(Conexion.Cadena))
+
+            {
+                var consulta = "delete *from usuario where Id_Usuario=@Id_Usuario";
+                var cmd = new MySqlCommand(consulta, cn);
+                cmd.Parameters.AddWithValue("@Id_Usuario", User.Id_Usuario);
+                cn.Open();
+                return Convert.ToBoolean(cmd.ExecuteNonQuery());
+
+            }
+
+        }
+    }
+
+    }
